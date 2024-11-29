cfbae91d366ba9fb04632f674acd2412e5baf119
MalithShehan <shehansandakalum2003@gmail.com>
2024-11-29 11:43:11 +0530

    Using ts file

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,32 @@
 import './App.css'
 import Item from './componant/Item.tsx'
+import {itemList} from './componant/Items.ts'
 // import ButtonComponent from './componant/ButtonComponent.tsx'
-function App() {
-    const itemList = [
-        {title: 'ABC', description: 'ABC description'},
-        {title: 'DEF', description: 'DEF description'},
-        {title: 'GHI', description: 'GHI description'},
 
-    ]
+function App() {
+    // const itemList = [
+    //     {title: 'ABC', description: 'ABC description'},
+    //     {title: 'DEF', description: 'DEF description'},
+    //     {title: 'GHI', description: 'GHI description'},
+    //
+    // ]
+
+    const itemArray = [];
+
+    for (const item of itemList) {
+        itemArray.push(<Item title={item.title} description={item.description}></Item>)
+    }
     return (
         <>
             {/*<ButtonComponent name = 'Add'></ButtonComponent>*/}
             {/*<ButtonComponent name = 'Update'></ButtonComponent>*/}
             {/*<ButtonComponent name = 'Delete'></ButtonComponent>*/}
 
-            <Item title={itemList[0].title} description={itemList[0].description}></Item>
-            <Item title={itemList[1].title} description={itemList[1].description}></Item>
-            <Item title={itemList[2].title} description={itemList[2].description}></Item>
+            {/*<Item title={itemList[0].title} description={itemList[0].description}></Item>*/}
+            {/*<Item title={itemList[1].title} description={itemList[1].description}></Item>*/}
+            {/*<Item title={itemList[2].title} description={itemList[2].description}></Item>*/}
+
+            {itemArray}
         </>
     )
 }
---
+++ src/componant/Items.ts
@@ -0,0 +1,6 @@
+export const itemList = [
+    {title: 'ABC', description: 'ABC description'},
+    {title: 'DEF', description: 'DEF description'},
+    {title: 'GHI', description: 'GHI description'},
+
+]
